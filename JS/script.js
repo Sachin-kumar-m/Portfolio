ae82b206bb40d77bd555ddba44211cc9c3c53c27
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-10-05 23:18:08 +0530

    fixed nav bar flickering issue

--- JS/script.js
+++ JS/script.js
@@ -4,25 +4,27 @@ let progressPercentage = []
 let navBar = document.querySelector(".navbar-nav")
 let navItems = document.getElementsByClassName("nav-item")
 let navigate = document.querySelector(".navbar")
-for (i = 0; i < skillName.length; i++){
+for (i = 0; i < skillName.length; i++) {
     progressPercentage.push(skillName[i].childNodes[3].innerText)
 }
 
-for (i = 0; i < progress.length; i++){
+for (i = 0; i < progress.length; i++) {
     progress[i].style.width = progressPercentage[i]
 }
 navBar.addEventListener("click", function (e) {
-    for (i = 0; i < navItems.length; i++){
+    for (i = 0; i < navItems.length; i++) {
         navItems[i].classList.remove("active")
     }
     e.target.classList.add("active")
-   
-    navigate.style.height = "10%"
     
-    if (e.target.innerText == "Home"){
+    if (e.target.innerText != "Home") {
+        navigate.style.height = "10%"
+    }
+
+    if (e.target.innerText == "Home") {
         setTimeout(function () {
             navigate.style.height = null
-        },500)
+        }, 500)
     }
 })
 // document.addEventListener("scroll",function () {
