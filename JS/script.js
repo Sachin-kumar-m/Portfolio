d8d6a71abf9826605263858cdb2201ca020a548a
sachin.kumar <sachin.kumar.maheshwarappa@gmail.com>
2023-10-13 19:56:56 +0530

    using news api added the content into HTML

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,37 @@ let formMessage = document.querySelector("#message")
 // eventlistener added on window to check the scroll and show the moveup only 
 // when scroll has crossed 2 sections
 
+async function newsApi() {
+    var url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=380539d5ba42403ea622428379772676';
+    let response = await fetch(url)
+    let json = await response.json()
+    let newsArticles = []
+    for (i = 0; i < 2; i++) {
+        newsArticles.push(json.articles[i])
+    }
+    // console.log(newsArticles)
+    updateUI(newsArticles);
+}
+newsApi()
+function updateUI(newsArticles) {
+    let newsTitles = document.querySelectorAll(".newstitle")
+    let newsDesciptions = document.querySelectorAll(".newsDescription")
+    let newsImages = document.querySelectorAll(".newsImage")
+    let newsDates = document.querySelectorAll(".newsDate")
+    let newsSources = document.querySelectorAll(".newsSource")
+    let links = document.querySelectorAll(".readMore")
+    // console.log()
+    for (i = 0; i < 2; i++) { 
+        newsTitles[i].innerHTML = newsArticles[i].title
+        newsDesciptions[i].innerHTML = newsArticles[i].description
+        newsImages[i].setAttribute("src", newsArticles[i].urlToImage)
+        newsDates[i].lastChild.data = newsArticles[i].publishedAt.substring(0, 9)
+        newsSources[i].lastChild.data = newsArticles[i].source.name
+        links[i].setAttribute("href",newsArticles[i].url)
+    }
+}
+
+
 window.addEventListener("scroll", function () {
     let showArrow = document.querySelector(".back-to-top")
     if (window.scrollY > 1266) {
@@ -28,7 +59,7 @@ setTimeout(() => {
     loader = document.getElementById("loader")
     loader.classList.remove("show")
     fullStack.classList.add("typewriter")
-}, 1500);
+},1500);
 
 
 for (i = 0; i < skillName.length; i++) {
